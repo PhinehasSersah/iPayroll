a97b40687887f42ef9bbdf823892ee8ac231ed1e
Daniel Bontii <daniel.bontii@amalitech.org>
2022-01-20 08:13:53 +0000

    refactored all department routes

--- server/main/app.js
+++ server/main/app.js
@@ -5,8 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-const hrRouter = require('./routes/hrRoutes');
-const pool = require('./db');
+const deptsRouter = require('./routes/departmentsRoutes');
 const app = express();
 
 app.use(cors());
@@ -16,80 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', hrRouter);
+app.use('/ipayroll/api/v1/departments', deptsRouter);
 
-app.get('/ipayroll/api/v1/departments', async (req, res) => {
-  try {
-    const departments = await pool.query('SELECT * FROM departments');
-    res.status(200).json(departments.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-app.get('/ipayroll/api/v1/departments/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const department = await pool.query(
-      'SELECT * FROM departments WHERE id=$1',
-      [id]
-    );
-    if (department.rowCount < 1) {
-      return res.status(404).json({ message: 'Invalid id' });
-    }
-    res.json(department.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-app.post('/ipayroll/api/v1/departments', async (req, res) => {
-  try {
-    const { name } = req.body;
-    if (!name) {
-      return res.json({ message: 'No department to add' });
-    }
-    const newDept = await pool.query(
-      'INSERT INTO departments (name) VALUES($1) RETURNING *',
-      [name]
-    );
-    res.status(201).json(newDept.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-app.put('/ipayroll/api/v1/departments/:id', async (req, res) => {
-  try {
-    console.log('putting....');
-    const { id } = req.params;
-    const { name } = req.body;
-    const department = await pool.query(
-      'UPDATE departments SET name=$1 WHERE id=$2',
-      [name, id]
-    );
-    if (department.rowCount < 1) {
-      return res.status(404).json({ message: 'No such deparment to update' });
-    }
-    res.json({ message: 'Successfully updated department' });
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-app.delete('/ipayroll/api/v1/departments/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const department = await pool.query('DELETE FROM departments WHERE id=$1', [
-      id,
-    ]);
-    if (department.rowCount < 1) {
-      return res.status(404).json({ message: 'No Department to delete' });
-    }
-    res.status(204).send();
-  } catch (err) {
-    console.error(err.message);
-  }
-});
 module.exports = app;
