18911cd4aa1e5036bd5a692b0abee45d35bcc70d
daniel-bontii <daniel.bontii@amalitech.org>
2022-01-28 14:15:58 +0000

    added generate and send payslip routes

--- server/main/routes/payslipsRoutes.js
+++ server/main/routes/payslipsRoutes.js
@@ -4,5 +4,8 @@ const payslipsController = require('../controllers/payslipsController');
 const router = express.Router();
 
 router.route('/:monthYear').get(payslipsController.getAllSlipInfo);
+router
+  .route('/send-slip')
+  .post(payslipsController.createPDF, payslipsController.sendPDDF);
 
 module.exports = router;
